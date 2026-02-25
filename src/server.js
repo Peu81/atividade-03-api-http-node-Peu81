a22dfc3d7a7000f34f5dfe6b984fa15b7f04ee5f
José Pedro Costa Duda <jpcd1@aluno.ifal.edu.br>
2026-02-24 22:03:37 -0300

    Criado metodo GET /chamados, retornando a listagem de chamados registrados em array_infos.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
-//Código do servidor
+import http from 'http';
+
+const array_infos = [{id_chamado: 1, nome_solicitante: "José Pedro"}] //responsavel por armazenar chamados.
+
+const server = http.createServer((req, res) => { // Cria o servidor e define a função para cada requisição
+
+  if (req.method === "GET" && req.url === "/health") { // Verifica rota GET /health
+    res.writeHead(200, { "Content-Type": "application/json" }); // Define status 200 e tipo JSON
+    res.end(JSON.stringify({ status: "ok" })); // Envia JSON e encerra
+    return; // Interrompe execução
+  }
+
+  if (req.method === "GET" && req.url.startsWith("/chamados")) { // Verifica rota GET /chamados
+    res.writeHead(200, { "Content-Type": "application/json" }); // Define status 200 e tipo JSON
+    res.end(JSON.stringify({"Listagem de chamados" : array_infos})); // Retorna lista de chamados
+    return; // Interrompe execução
+  }
+
+
+  res.end(); // Finaliza resposta
+});
+
+server.listen(3000, () => { // Inicia o servidor na porta 3000
+  console.log("Servidor HTTP executando na porta 3000"); // Log informativo
+});
