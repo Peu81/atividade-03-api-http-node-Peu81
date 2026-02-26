45d7330d58002d50208c1e8b94065056798d5c2e
Neto <jpcd1@aluno.ifal.edu.br>
2026-02-26 00:15:47 -0300

    refactor: função 'salvarChamado' em 'chamados.js'

--- src/data/chamados.js
+++ src/data/chamados.js
@@ -1,1 +1,11 @@
-export const array_infos = [{id_chamado: 1, nome_solicitante: "Joana Fragoso"}]; //responsavel por armazenar chamados.
+export const array_infos = [{id_chamado: 1, nome_solicitante: "Joana Fragoso", 
+    descricao: "Reinicializações Inesperadas", 
+    prioridade: "alta"}]; //responsavel por armazenar chamados.
+
+let novoID = 2;
+
+export const salvarChamado = (novoChamado) => {
+    const chamadoId = {id: novoID++, ...novoChamado};
+    array_infos.push(chamadoId)
+    return chamadoId;
+};
