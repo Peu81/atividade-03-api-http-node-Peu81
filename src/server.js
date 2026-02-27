307bee351255d8567d2a4d67398b59e4f244d0e3
José Pedro <jpcd1@aluno.ifal.edu.br>
2026-02-27 10:12:09 -0300

    refactor: mudanças lógicas nos métodos GET '/chamados' e '/chamados/'

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const array_infos = [{id_chamado: 1, nome_solicitante: "Joana Fragoso",
     descricao: "Reinicializações Inesperadas", 
     prioridade: "alta"}]; //responsável por armazenar chamados.
 
-let incrementoId = 2
-
 const server = http.createServer((req, res) => { // Cria o servidor e define a função para cada requisição
 
   if (req.method === "GET" && req.url === "/health") { // Verifica rota GET /health
@@ -14,22 +12,16 @@ const server = http.createServer((req, res) => { // Cria o servidor e define a f
     return; // Interrompe execução
   }
 
-  if (req.method === "GET" && req.url.startsWith("/chamados")) { // Verifica rota GET /chamados 
-    res.writeHead(200, { "Content-Type": "application/json" }); // Define status 200 e tipo JSON
-    res.end(JSON.stringify(array_infos)); // Retorna lista de chamados registrados em array_infos
-    return; // Interrompe execução
-  }
-
   if (req.method === "GET") {
     const fatiaUrl = req.url.split("/chamados/")
   
-    if (fatiaUrl.length === 2 && fatiaUrl[2] !== "") {
-      const id = Number(fatiaUrl[2]);
+    if (fatiaUrl.length === 2 && fatiaUrl[1] !== "") {
+      const id = Number(fatiaUrl[1]);
 
       //verifica se não é valor numérico
       if (isNaN(id)) { 
-        res.writeHead(400, {"content-type": "application/json"}); //Define status 400 e tipo JSON 
-        res.end(JSON.stringify({erro: "Bad Request"})); // Mensagem de erro
+        res.writeHead(404, {"content-type": "application/json"}); //Define status 400 e tipo JSON 
+        res.end(JSON.stringify({erro: "Rota inválida"})); // Mensagem de erro
         return;
       };
 
@@ -43,13 +35,20 @@ const server = http.createServer((req, res) => { // Cria o servidor e define a f
       };
 
       res.writeHead(200, {"Content-Type": "application/json"}); // Define status 200 e tipo JSON
-      res.end(JSON.stringify({chamado})) // Retorna o chamado de acordo com o ID fornecido
+      res.end(JSON.stringify(chamado)) // Retorna o chamado de acordo com o ID fornecido
       return;
 
     }
   }
 
-  res.end(); // Finaliza resposta
+  if (req.method === "GET" && req.url.startsWith("/chamados")) { // Verifica rota GET /chamados 
+    res.writeHead(200, { "Content-Type": "application/json" }); // Define status 200 e tipo JSON
+    res.end(JSON.stringify(array_infos)); // Retorna lista de chamados registrados em array_infos
+    return; // Interrompe execução
+  }
+  
+  res.writeHead(404, {"Content-Type": "application/json"});
+  res.end(JSON.stringify({erro: "Rota desconhecida"})); // Finaliza resposta para rota não encontrada
 });
 
 server.listen(3000, () => { // Inicia o servidor na porta 3000
